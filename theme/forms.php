b61bcd5afc789b955b38f6d876e2ec291a1d6877
LuskasGT <lucas.g.theodooro@gmail.com>
2023-09-01 07:59:51 -0300

    trocando endereço por telefone

--- theme/forms.php
+++ theme/forms.php
@@ -1,6 +1,7 @@
 <?php 
 if(!empty($_POST["email"])){
 
+   
 $email = addslashes($_POST["email"]);
 $nome = addslashes($_POST["nome"]);
 $endereco = addslashes($_POST["endereco"]);
@@ -11,16 +12,22 @@ $subject = "Contato para adoção";
 $body = "email: ".$email. "\r\n".
         "nome: ".$nome. "\r\n".
         "endereco: ".$endereco. "\r\n".
-        "telefone: ".$endereco;
+        "telefone: ".$telefone;
 
 $header = "From: lucas.g.theodooro@gmail.com"."\r\n".
  "Reply-To:" .$email."\r\n".
  "X=Mailer:PHP/".phpversion();
 
+ // Adicionando para o SMTP responder direito para a porta 25
+ini_set("SMTP","smtp.example.com" );
+ini_set("smtp_port","587" );
+
  if(mail($to,$subject,$body,$header)){
     echo("Email enviado com sucesso");
+    echo $teste;
  }else{
     echo("Email não foi enviado");
+    echo $teste;
  }
 
 
