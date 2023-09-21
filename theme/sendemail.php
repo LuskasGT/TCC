437fd7f4b873632dae91538fef4a858a9f3b9a80
joaoluis221 <lucas.g.theodooro@gmail.com>
2023-09-21 14:03:36 -0300

    deixando nota

--- theme/sendemail.php
+++ theme/sendemail.php
@@ -8,7 +8,7 @@ require 'lib/vendor/autoload.php'; // If you're using Composer (recommended)
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
 
-$email = new \SendGrid\Mail\Mail(); 
+$email = new \SendGrid\Mail\Mail();
 $email->setFrom("lucas.g.theodooro@gmail.com", "Lucas TCC");
 $email->setSubject("Adoção de animais - Lucio Pelizer");
 $email->addTo("lucas.g.theodooro@gmail.com", "Lucas TCC");
@@ -16,7 +16,8 @@ $email->addContent("text/plain", "Enviado com sucesso(acredito)");
 $email->addContent(
     "text/html", "<strong>Enviado com sucesso(acredito)</strong>"
 );
-$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+$sendgrid = new \SendGrid(getenv('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'));
+                                //   SRjguPO4QpSF1RekkLA2fg
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
@@ -25,3 +26,5 @@ try {
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
+
+// pegar do código do jonas e fazer a troca, preencher os campos tio email pegar do html e passsar para o php
